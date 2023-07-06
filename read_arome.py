538a99a9b2fbce8461b37ac390a2eac3096212be
Cloe DAVID <cloe.david.80@gmail.com>
2023-07-06 17:47:22 +0200

    Add a function to get microphysics concentrations (no LIMA options yet) + removing all call to config file (pass the parameters via functions attributes instead)

--- read_arome.py
+++ read_arome.py
@@ -7,57 +7,43 @@ Created on Tue Apr 11 09:55:15 2023
 """
 
 import numpy as np
-
 import epygram
 import bronx
-
-
-import operad_conf as cf
 import read_arome_lib as arolib
 
 epygram.init_env()
 
 
-## ---- to be included in conf file
-#micro="ICE3"
-##rep="/home/augros/DONNEES/AROME/20220816/PEAROME/R09/"
-#rep="/cnrm/precip/users/augros/DONNEES/AROME/"
-#file="historic.arome.franmg-01km30+0008:00.fa"
-##rep="/home/augros/DONNEES/AROME/20220816/3DVARFR/T00/" 
-##file="historic.arome.franmg-01km30+0017:00.fa"
-#lat_min,lat_max=47.0,50.
-#lon_min,lon_max=1.,4.
-#modelfile=rep+file
-# -------------------------------------
-
-
 #================= Read Arome variables =======================================
 """
-Read Arome 3D variables in ncfile: pressure, temperature, hydrometeor contents 
-input : micro,modelfile,lon_min,lon_max,lat_min,lat_max
-output: M, Tc, CC, CCI, lon, lat, Z
-
-
+Read Arome 3D variables in ncfile: pressure, temperature, hydrometeor contents
+and concentrations 
+INPUT : microphysics,modelfile,lon_min,lon_max,lat_min,lat_max
+OUTPUT: M, Tc, CC, CCI, lon, lat, Z
 """
-def read_arome(micro,timestr):   
+
+def read_arome(modelfile: str,
+               microphysics: str,
+               hydrometeors_list: list,
+               lonmin: float = -5.2,
+               lonmax: float = 8.3,
+               latmin: float = 41.3,
+               latmax: float = 51.15,
+               ):   
     
     epygram.init_env()
-    # ======== Open file 
-    modelfile=cf.pathmodel+cf.filestart+timestr+".fa"
-    
     
     print("Reading AROME fa file: ",modelfile)
     ficA = epygram.formats.resource(modelfile, openmode = 'r', fmt = 'FA')
     ps = ficA.readfield('SURFPRESSION')
     
-    
-    # === Infos
+    # === Infos === #
     #ficA.listfields()
     #ficA.what()
     
-    # ======= Zoom
-    imin,jmin=(np.round(ps.geometry.ll2ij(cf.lon_min,cf.lat_min)).astype(int))
-    imax,jmax=(np.round(ps.geometry.ll2ij(cf.lon_max,cf.lat_max)).astype(int))
+    # === Zoom === #
+    imin,jmin=(np.round(ps.geometry.ll2ij(lonmin,latmin)).astype(int))
+    imax,jmax=(np.round(ps.geometry.ll2ij(lonmax,latmax)).astype(int))
     
     #Fichier FA avec SubdomainResource
     ficsubdo = epygram.resources.SubdomainResource(resource=ficA, openmode='r', name='Subdomain',
@@ -69,11 +55,10 @@ def read_arome(micro,timestr):
     [p, psurf, pdep, phis, A, B, lon, lat]=arolib.get_geometry(ficA,ficsubdo)
       
     # ======== Hydrometeor contents and temperature
-    [M, T, R]=arolib.get_contents_t(ficsubdo, p)
-    
+    [M, T, R]=arolib.get_contents_and_T(ficsubdo, p, hydrometeors_list)
     Tc=T-273.15
-    CC=np.empty(Tc.shape)
-    CCI=cf.CCIconst*np.ones(Tc.shape)
+    [CC , CCI] = arolib.get_concentrations(ficsubdo, p, Tc,microphysics,hydrometeors_list)
+    
         
     # ========= Altitude z of each level
     Z=arolib.get_altitude(A, B, T, p, pdep, psurf, phis, R)
@@ -93,7 +78,7 @@ def read_arome(micro,timestr):
 if __name__ == '__main__':
 
     # ---- to be included in conf file
-    micro="ICE3"
+    microphysics="ICE3"
     #rep="/home/augros/DONNEES/AROME/20220816/PEAROME/R09/"
     rep="/cnrm/precip/users/augros/DONNEES/AROME/"
     file="historic.arome.franmg-01km30+0008:00.fa"
