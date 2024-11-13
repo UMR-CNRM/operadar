1c9faf44b456b6cd5939d68509493fd53df2e5c1
Cloé David <cloe.david@meteo.fr>
2024-11-13 19:28:35 +0100

    Update of the config example base file

--- configFiles/conf_Arome_0.py
+++ configFiles/conf_Arome_0.py
@@ -11,26 +11,24 @@
 """
 import datetime as dt
 
-# ==========  Model simulation options ===============
-LIMToption="" #"" or "cstmu" the model variables are taken from LIMT simulation # but a constant mu is applied in the PSD  for the dpol variables calculation 
-CCIconst=800.
-htypes_model=['vv','cc','rr','ii','ss','gg'] # available model variables
-list_types_tot = ['rr','ii','ss','gg','wg']
-
-MixedPhase="Fwposg" # 'Tpos' or 'Fwpos' or 'Fwposg' #
- 
-singletype=False #False #True # if True: computes dpol var for each type
-
-n_interpol = 32      # nb bornes to interpol (2**5: min et max pour LAM, ELEV, T, M, Fw)
-
+# ==========  Characteristics of the data (AROME or MESONH files) ===============
+htypes_model=['vv','cc','rr','ii','ss','gg']    # available model variables
+n_moments_model = [1,1,1,1,1,1]                 # corresponding moments
 step = dt.timedelta(minutes=5)
-save_npz    = False # save file(s) in .npz format
-save_netcdf = True # save file(s) in .nc format (netcdf4)
+ 
 
-# Radar options
-distmax_rad = 255.*1000 #150*1000 # Distance max des données radar dont on calcule les pseudo-observations
-alt_max = 15000. # Altitude max des données radar utilisées pour le calcul des pseudo-observations
-radarloc="center" # radar location: center or latlon (if latlon ==> to be defined below buy user)
+# ========== Forward operator options ===============
+# Model options
+LIMToption=""                               #"" or "cstmu" the model variables are taken from LIMT simulation # but a constant mu is applied in the PSD  for the dpol variables calculation 
+n_interpol = 32                             # nb bornes to interpol (2**5: min et max pour LAM, ELEV, T, M, Fw)
+MixedPhase="Fwposg"                         # 'Tpos' or 'Fwpos' or 'Fwposg'
+singletype=False                            # if True: computes dpol var for each type
+list_types_tot = ['rr','ii','ss','gg','wg'] # only for T-matrix computation
+
+# Radar simulation options
+distmax_rad = 255.*1000     # maximum radius of the radar data to compute pseudo-observations
+alt_max = 15000.            # maximum height of the radar data used in the computation of the pseudo-observations
+radarloc="center"           # radar location: center or latlon (if latlon ==> to be defined below buy user)
 
 
 # ========== Directories / file's name ========= #
@@ -42,24 +40,26 @@ repTmat   = "/cnrm/precip/SAVE/davidcl/THESE/DPOLSIMUL"
 pathTmat = repTmat+"/OUTPUT/"
 
 # Model files paths
-arome_commonPath  = f"/cnrm/precip/users/davidcl/expeOLIVE/arome/3dvarfr/" # for recurrent nomenclature
-arome_experience_name = None
-arome_commonFilename = "historic.arome.franmg-01km30+00"            # idem
+arome_commonPath  = f"/cnrm/precip/users/davidcl/expeOLIVE/arome/3dvarfr/"  # for recurrent nomenclature
+arome_commonFilename = "historic.arome.franmg-01km30+00"                    # idem
+arome_experience_name = None                                                # set None if multiple
 outPath = f"/cnrm/precip/SAVE/davidcl/THESE/operadar_files"
 csvPath = "/cnrm/precip/SAVE/davidcl/THESE/studyCases_from_ods.csv"
 
 # ========== Constants =======================
 RT = 6371.229*10**3 # Earth radius
+CCIconst=800.
+
 
 # =========== CSV SETUP ==============
 """ Please explicit the name of your csv columns based on the given example hereafter
 
     - Domain :
-        Either your domain is written within one column with the format [lonmin,lonmax,latmin,latmax]
-        --> provide the name of the column in the following format :  "name_of_my_column"
-        Or the latitude/longitude min/max are written in 4 distinct columns
-        --> provide the name of the columns in the following format : ["lonmin","lonmax","latmin","latmax"]
-            (the names can differ, but needs to be written in the same order as the example)
+        * Either your domain is written within one column with the format [lonmin,lonmax,latmin,latmax]
+          --> provide the name of the column in the following format :  "name_of_my_column"
+        * Or the latitude/longitude min/max are written in 4 distinct columns
+          --> provide the name of the columns in the following format : ["lonmin","lonmax","latmin","latmax"]
+              (the names can differ, but needs to be written in the same order as the example)
 """
 csv_time_columns = ["model_start_time", "model_end_time"]
 csv_model_run_column = "model_run" #set to None in case of a MesoNH file
