0d89783ca5a31e21ba63cec144022166f392d6b5
Cloé David <cloe.david@meteo.fr>
2025-02-04 20:28:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='operad',
+    name='operadar',
     version='0.0',
-    packages=find_packages("operadar"),
-    description='Radar forward operator',
     author='C. Augros, C. David, T. LeBastard',
+    description='Radar forward operator developped at the National Centre for Meteorological Research, France',
+    url='https://github.com/UMR-CNRM/operadar.git'
+    packages=find_packages("operadar"),
     install_requires=[],
 )
