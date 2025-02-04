2004af78bc987d4ec947064598d967d76a193cac
Cloé David <cloe.david@meteo.fr>
2025-02-04 17:37:43 +0100

    Clean and update the executable with the new version of operad.py

--- exec_operad.sh
+++ exec_operad.sh
@@ -1,53 +1,30 @@
 #!/bin/bash
 
 [ "$1" = "-h" -o "$1" = "--help" ] && echo "
-  Before running exec_operad.sh code, you need to:
-  * create a case study file in ./study_cases/
-  * create a configuration file in ./configFiles/
+Before running exec_operad.sh code, you need to create a configuration file in ./configFiles/ based on the template provided.
+
+This script takes 3 arguments in this order :
+  1 - the filename to read (+ additional tree structure if not provided in the configuration file)
+  2 - date in the yyyymmdd format
+  3 - name of the configuration file
+
+----------
+ Examples
+----------
+ * With input_file_dir  = "/cnrm/precip/users/davidcl/expeOLIVE/arome/3dvarfr/GO2V/20220818T1200P/forecast/" in config file :
+   >>> ./exec_operad.sh historic.arome.franmg-01km30+0007:00.fa 20220818 conf_Arome_with_ICE3.py
+
+ * With input_file_dir  = "/home/davidcl/Programmation/expeOLIVE/" in config file :
+   >>> ./exec_operad.sh LIMA_simus_Arome/00Z/20220620/historic.arome.franmg-01km30+0003:00.fa 20220620 conf_Arome_with_LIMA_chaud.py
   
-  This script takes 4 arguments in this order :
-      1 - Arome or MesoNH
-      2 - date into the yyyymmdd format or "all"
-      3 - microphysics scheme name in capital letter (ICE3, ICE4, LIMA, LIMAAG)
-      4 - Config file specifying directories and forward operator options 
-  _________________________________________
-  Examples :
-   >>> ./exec_operad.sh MesoNH 20220818 ICE3 conf_MesoNH_ICE3_CORSEbe.py
-   >>> ./exec_operad.sh MesoNH 20220818 LIMA conf_MesoNH_LIMA_CORSEbe.py
-   >>> ./exec_operad.sh Arome 20220818 ICE3 conf_Arome_ICE3_CORSEbe.py
-   >>> ./exec_operad.sh MesoNH 20220818 LIMAAG conf_MesoNH_LIMAAG_CORSEbe.py
-   >>> ./exec_operad.sh Arome 20250122 LIMA conf_Arome_LIMAC_VISULIMA.py
-   >>> ./exec_operad.sh Arome 20250122 LIMA conf_Arome_LIMA_VISULIMA.py
-   >>> ./exec_operad.sh Arome 20250122 ICE3 conf_Arome_ICE3_VISULIMA.py
-  _________________________________________  
-  STEPS :
-     a) Creates or check the existance of the directory where all nohupFiles.txt will be saved (nohupOUT)
-     b) Copy the right configuration file thanks to arguments 4
-     c) Run operad.py and save the prompt output into ./nohupOUT/nohupFile.txt
-     d) Return in ./process_id_historic.txt the process id (for killing purpose)
-  
-  Note : process_id_historic.txt keep in memory all process id + the function arguments and the date/time the program was launched.
-         You may want to manually erase some old lines or delete the file to clear process_id_historic.txt
+ * With input_file_dir  = "/home/cnrm_other/ge/mrmp/davidcl/MNH-V5-7-0/MY_RUN/" in config file :
+   >>> ./exec_operad.sh LIMAH.1.SEG01.003.nc 20220818 conf_MesoNH_LIMA_CORSEbe.py
+_____________________________________________________________________________________________________________________________________
 " && exit
 
-module use ~mary/public/modulefiles
-module load epygram
 
-if [ ! -d ./nohupOUT ]; then
-  mkdir nohupOUT
-fi
+cp ./configFiles/$3 operadar/operad_conf.py
 
-cp ./configFiles/$4 operad_conf.py
+currentTime=`date +"%F_%R"`
 
-currentTime=`date +"%F %R"`
-
-# --- For tests -----
-python3 -i operad.py $1 $2 $3
-
-# --- For multiple times running ---
-#nohup python3 -u operad.py $1 $2 $3 >nohupOUT/out_$1_$2_$3.txt 2> nohupOUT/err_$1_$2_$3.txt &
-
-echo ${currentTime} $! $1 $2 $3 >> process_id_historic.txt
-
-echo program output redirected to ./nohupOUT/out_$1_$2_$3.txt
-echo corresponding process id written in ./process_id_historic.txt
+python3 -u operadar/operad.py $1 $2
