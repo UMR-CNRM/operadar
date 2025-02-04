7baa98436347bd4ddb516740d8effc2bfac1b8f7
Cloé David <cloe.david@meteo.fr>
2025-02-04 15:46:37 +0100

    Start shaping package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='operad',
+    version='0.0',
+    packages=find_packages("operadar"),
+    description='Radar forward operator',
+    author='C. Augros, C. David, T. LeBastard',
+    install_requires=[],
+)
