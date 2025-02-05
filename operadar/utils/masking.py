2843618340a2c6b4e66f8da661d3291c624e2eeb
Cloé David <cloe.david@meteo.fr>
2025-02-05 16:17:10 +0100

    Unit addition

--- operadar/utils/masking.py
+++ operadar/utils/masking.py
@@ -7,7 +7,7 @@ from operadar.utils.make_links import link_keys_with_available_hydrometeors
 
 def mask_precipitations(contents:dict[np.ndarray],expMmin:float,hydrometeorMoments:dict[int]):
     "Mask grid points where the total precipitations are below expMmin threshold."
-    print("Masking precipitations where total <",10**expMmin)
+    print("Masking precipitations where total <",10**expMmin,"kg/m3.")
     hydrometeors_model = link_keys_with_available_hydrometeors(hydrometeorMoments,datatype='model')
     total_content=np.sum(contents[h] for h in hydrometeors_model)
     mask_precip=(total_content>10**expMmin)  
