cc6683089de0c51be50d60cf47c7fafa3382bcb0
Cloé David <cloe.david@meteo.fr>
2025-02-13 14:37:25 +0100

    Update

--- exec_operad.sh
+++ exec_operad.sh
@@ -11,13 +11,13 @@ This script takes 3 arguments in this order :
 ----------
  Examples
 ----------
- * With input_file_dir  = "/cnrm/precip/users/davidcl/expeOLIVE/arome/3dvarfr/GO2V/20220818T1200P/forecast/" in config file :
+ * With input_directory  = "/cnrm/precip/users/davidcl/expeOLIVE/arome/3dvarfr/GO2V/20220818T1200P/forecast/" in config file :
    >>> ./exec_operad.sh historic.arome.franmg-01km30+0007:00.fa 20220818 conf_Arome_with_ICE3.py
 
- * With input_file_dir  = "/home/davidcl/Programmation/expeOLIVE/" in config file :
+ * With input_directory  = "/home/davidcl/Programmation/expeOLIVE/" in config file :
    >>> ./exec_operad.sh LIMA_simus_Arome/00Z/20220620/historic.arome.franmg-01km30+0003:00.fa 20220620 conf_Arome_with_LIMA_chaud.py
   
- * With input_file_dir  = "/home/cnrm_other/ge/mrmp/davidcl/MNH-V5-7-0/MY_RUN/" in config file :
+ * With input_directory  = "/home/cnrm_other/ge/mrmp/davidcl/MNH-V5-7-0/MY_RUN/" in config file :
    >>> ./exec_operad.sh LIMAH.1.SEG01.003.nc 20220818 conf_MesoNH_LIMA_CORSEbe.py
 _____________________________________________________________________________________________________________________________________
 " && exit
