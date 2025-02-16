90791318c0da5d703098aa0ef4bdd6b01a035a82
Cloé David <cloe.david@meteo.fr>
2025-02-16 22:21:05 +0100

    Template is now ready to be used and customized.

--- configFiles/conf_template.py
+++ configFiles/conf_template.py
@@ -9,36 +9,32 @@
  *
 """
 
-# Model name : can be 'Arome' or 'MesoNH'
-model = 'Arome'
+# ----- INPUT file path (filename will be used to name the output file)
+input_filePath  = "home/my_input_folder/" 
 
-# Microphysics scheme name : can be 'ICE3', 'ICE4' or 'LIMA' + an accepted name extension (e.g. 'LIMA_noHail' or 'ICE3_CIBU_moins')
-micro_scheme = 'ICE3'
+# ----- OUPUT file(s) directory
+outPath = f"/home/my_output_folder/"
 
-# Number of moments for each hydrometeor of the microphysics scheme
-hydrometeors_moments = {'cc':1,'rr':1,'ss':1,'gg':1,'ii':1,'wg':1}
-
-# Subdomain : written as [lon_min,lon_max,lat_min,lat_max] or None (will use all the points in the file)
-subDomain = [-2.61,2.1,42.68,46.5]
-
-# Mixed phase simulation : can be 'T_pos' or 'Fw_pos' or 'Fw_posg'. Please, have a look at the README beforehand.
-MixedPhase = 'Fw_posg'
-
-# Additional output : if True, compute the dual-pol variables for each hydrometeor class and save the resultant netcdf (1 file/hydrometeor class)
-save_netcdf_single_hydrometeor = False
-
-# Tmatrix directory (TmatCoefInt_SCXW) path
+# ----- Tmatrix directory (TmatCoefInt_SCXW) path
 path_Tmatrix = "/my_path/TmatCoefInt_SCXW/"
 
-# INPUT file(s) directory
-input_directory  = "/home/my_input_folder/" 
+# ----- Model name : can be 'Arome' or 'MesoNH'
+model = 'Arome'
 
-# If applicable, to use if experiences are separated in different sub-directories. Else set to ''.
-experience_name = 'whatever_name'
+# ----- Microphysics scheme name : can be 'ICE3', 'ICE4' or 'LIMA' + an accepted name extension (e.g. 'LIMA_noHail' or 'ICE3_CIBU_moins')
+micro_scheme = 'LIMA_exp_GNZR'
 
-# OUPUT file(s) directory
-outPath = "/home/my_output_folder/"
+# ----- Number of moments for each hydrometeor of the microphysics scheme
+hydrometeors_moments = {'cc':2,'rr':2,'ss':1,'gg':1,'ii':2,'wg':1}
 
+# ----- Subdomain : written as [lon_min,lon_max,lat_min,lat_max] or None (will use all the points in the file)
+subDomain = None
+
+# ----- Mixed phase simulation : can be 'T_pos' or 'Fw_pos' or 'Fw_posg'. Please, have a look at the README beforehand.
+MixedPhase = 'Fw_posg'
+
+# ----- Additional output : if True, compute the dual-pol variables for each hydrometeor class and save the resultant netcdf (1 file/hydrometeor class)
+save_netcdf_single_hydrometeor = False
 
 # ----- Radar simulation options ----- #
 radar_band = 'C'                       # radar band (C, X, S, W or K)
@@ -51,3 +47,4 @@ radarloc="center"                      # radar location: 'center' or [lat_radar,
 n_interpol = 32 # nb bornes to interpol (2**5: min et max pour LAM, ELEV, T, M, Fw)
 RT = 6371.229*10**3 # Earth radius constant
 CCIconst=800. # Ice concentration constant for one moment
+LIMToption=""
