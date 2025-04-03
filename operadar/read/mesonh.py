596b39c7211823a989820d08b387c1547eccae6f
Cloé David <cloe.david@meteo.fr>
2025-04-03 15:45:23 +0200

    Allow to slice a subdomain based on grid indices for idealized MesoNH cases

--- operadar/read/mesonh.py
+++ operadar/read/mesonh.py
@@ -46,118 +46,112 @@ def read_mesonh(filePath: str,micro: str,subDomain:list[float]|None,
     if verbose : deb=tm.time()
     mnh_file = Dataset(filePath,'r')
     if verbose : print('\t\tLoaded file in',round(tm.time()-deb,3),'seconds'); deb=tm.time()
+    
+    #add check variables
+    
+    if subDomain != None :
+        i_min, i_max, j_min, j_max = get_subdomain_indices(mnhFile=mnh_file,
+                                                           subDomain=subDomain,
+                                                           real_case=real_case,
+                                                           )
+        if verbose : print('\t\tSubdomain indices extracted in',round(tm.time()-deb,6),'seconds'); deb=tm.time()
+    else:
+        i_min, i_max, j_min, j_max = 0, -1, 0, -1
+        if verbose : print('\t\tNo subdomain provided, will use all the points.'); deb=tm.time()
 
-    X, Y, Z = get_geometry(mnhFile=mnh_file,
-                           real_case=real_case,
-                           )
+    X, Y, Z, LAT, LON = get_geometry(mnhFile=mnh_file,
+                                     real_case=real_case,
+                                     i_min=i_min, i_max=i_max,
+                                     j_min=j_min, j_max=j_max,
+                                     )    
     if verbose : print('\t\tGot geometry in',round(tm.time()-deb,6),'seconds');deb=tm.time()
     
-    if real_case:
-        LAT = mnh_file.variables['latitude'][:]
-        LON = mnh_file.variables['longitude'][:]
-        if subDomain != None :
-            lonmin, lonmax, latmin, latmax = extract_subdomain(mnhFile=mnh_file,
-                                                               longitude_field=LON,
-                                                               latitude_field=LAT,
-                                                               subDomain=subDomain,
-                                                               )
-            if verbose : print('\t\tSubdomain extracted in',round(tm.time()-deb,6),'seconds'); deb=tm.time()
-        else :
-            if verbose : print('\t\tNo subdomain provided, will use all the points.'); deb=tm.time()
-    else:
-        LAT=float('nan')
-        LON = float('nan')
-        if verbose : print('\t\tIdealized case, will use all the points.'); deb=tm.time()
-    
-    Tc, rho3D = get_temperature_and_density(mnhFile=mnh_file)
+    Tc, rho3D = get_temperature_and_density(mnhFile=mnh_file,
+                                            i_min=i_min, i_max=i_max,
+                                            j_min=j_min, j_max=j_max,
+                                            )
     if verbose : print('\t\tGot temperature and dry air density in',round(tm.time()-deb,6),'seconds');deb=tm.time()
     
     hydromet_list = link_keys_with_available_hydrometeors(hydrometeorMoments=hydrometeorMoments, datatype='model')
     name_hydro = link_varname_with_mesonh_name()
     
-    M = get_contents(mnhFile=mnh_file,
-                     hydrometeors=hydromet_list,
-                     name_var_hydro=name_hydro,
-                     temperature=Tc,
-                     rho=rho3D,
+    M = get_contents(mnhFile=mnh_file, hydrometeors=hydromet_list,
+                     name_var_hydro=name_hydro, temperature=Tc, rho=rho3D,
+                     i_min=i_min, i_max=i_max,
+                     j_min=j_min, j_max=j_max,
                      )
     if verbose : print('\t\tGot 3D contents in',round(tm.time()-deb,6),'seconds');deb=tm.time()
     
-    Nc = get_concentrations(mnhFile=mnh_file,
-                            microphysics_scheme=micro,
-                            hydrometeors=hydromet_list,
-                            temperature=Tc,
-                            rho=rho3D,
+    Nc = get_concentrations(mnhFile=mnh_file, microphysics_scheme=micro,
+                            hydrometeors=hydromet_list, temperature=Tc, rho=rho3D,
+                            i_min=i_min, i_max=i_max,
+                            j_min=j_min, j_max=j_max,
                             )
     if verbose : print('\t\tGot 3D number concentrations in',round(tm.time()-deb,6),'seconds');deb=tm.time()
 
-    # Cropping data if necessary (in a function later ?)
-    if real_case and subDomain != None :
-        M_zoom,Nc_zoom={},{}
-        for key in hydromet_list:
-            M_zoom[key]=M[key][:,lonmin:lonmax,latmin:latmax]
-            Nc_zoom[key]=Nc[key][:,lonmin:lonmax,latmin:latmax]
-        
-        Tc_zoom=Tc[:,lonmin:lonmax,latmin:latmax]
-        LAT_zoom=LAT[lonmin:lonmax,latmin:latmax]
-        LON_zoom=LON[lonmin:lonmax,latmin:latmax]
-        X_zoom=X[latmin:latmax]
-        Y_zoom=Y[lonmin:lonmax]
-        Z_zoom=Z[:,lonmin:lonmax,latmin:latmax]
-        
-        return X_zoom, Y_zoom, Z_zoom, LON_zoom, LAT_zoom, M_zoom, Nc_zoom, Tc_zoom 
-    else:
-        return X, Y, Z, LON, LAT, M, Nc, Tc
+    return X, Y, Z, LON, LAT, M, Nc, Tc
 
 
 
-def get_geometry(mnhFile:Dataset,real_case:bool):
-    X = mnhFile.variables['XHAT'][:]
-    Y = mnhFile.variables['YHAT'][:]
+def get_geometry(mnhFile:Dataset,
+                 real_case:bool,
+                 i_min:int, i_max:int,
+                 j_min:int, j_max:int,
+                 ):
+    X = mnhFile.variables['XHAT'][:][j_min:j_max]
+    Y = mnhFile.variables['YHAT'][:][i_min:i_max]
     ZHAT = mnhFile.variables['ZHAT'][:] # height above ground (m)
     if real_case :
-        ZS = mnhFile.variables['ZS'][:] # altitude (m) of model surface (ground)
+        ZS = mnhFile.variables['ZS'][:][i_min:i_max,j_min:j_max] # altitude (m) of model surface (ground)
+        LAT = mnhFile.variables['latitude'][:][i_min:i_max,j_min:j_max]
+        LON = mnhFile.variables['longitude'][:][i_min:i_max,j_min:j_max]
     else :
         ZS = np.zeros((X.shape,Y.shape)) # is null for idealized cases
+        LAT =float('nan')
+        LON = float('nan')
     Z_3D = np.empty((ZHAT.shape[0],ZS.shape[0],ZS.shape[1]))
     for level in range(ZHAT.shape[0]):
         Z_3D[level,:,:]=ZS[:,:]+ZHAT[level]
-    return X, Y, Z_3D
+    return X, Y, Z_3D, LAT, LON
 
 
 
-def extract_subdomain(mnhFile:Dataset,
-                      longitude_field:np.ndarray,
-                      latitude_field:np.ndarray,
-                      subDomain:list[float],
-                      ):
-    lon_min, lon_max, lat_min, lat_max = get_lat_lon_from_subdomain(subDomain)
-    mask__zoom = ((longitude_field>lon_min)
-                 &(longitude_field<lon_max)
-                 & (latitude_field>lat_min)
-                 & (latitude_field<lat_max)
-                 )
-    [ilon,jlat]=np.where(mask__zoom)
-    i_lonmin,i_lonmax=np.nanmin(ilon),np.nanmax(ilon)
-    j_latmin,j_latmax=np.nanmin(jlat),np.nanmax(jlat)
-    return i_lonmin, i_lonmax, j_latmin, j_latmax
+def get_subdomain_indices(mnhFile:Dataset,
+                          subDomain:list[float],
+                          real_case:bool,
+                          ):
+    if real_case:
+        LAT = mnhFile.variables['latitude'][:]
+        LON = mnhFile.variables['longitude'][:]
+        lon_min, lon_max, lat_min, lat_max = get_lat_lon_from_subdomain(subDomain)
+        mask_zoom = ((LON>lon_min) &(LON<lon_max) & (LAT>lat_min) & (LAT<lat_max) )
+        [ilon,jlat]=np.where(mask_zoom)
+        i_min, i_max = np.nanmin(ilon), np.nanmax(ilon)
+        j_min, j_max = np.nanmin(jlat), np.nanmax(jlat)  
+    else :
+        i_min, i_max = subDomain[0], subDomain[1]
+        j_min, j_max = subDomain[2], subDomain[3]
+    return i_min, i_max, j_min, j_max
 
 
 
-def get_temperature_and_density(mnhFile:Dataset):
+def get_temperature_and_density(mnhFile:Dataset,
+                                i_min:int, i_max:int,
+                                j_min:int, j_max:int,
+                                ):
     # Temperature
-    p=mnhFile.variables['PABST'][0,:,:,:]
-    p[np.where(p==999.)]=float('nan')
-    Th=mnhFile.variables['THT'][0,:,:,:]
-    Th[np.where(Th==999.)]=float('nan')
-    Tc=Th*((p/100000)**(0.4/1.4))-273.15
+    p=mnhFile.variables['PABST'][0,:,:,:][:,i_min:i_max,j_min:j_max]
+    p[np.where(p==999.)] = float('nan')
+    Th=mnhFile.variables['THT'][0,:,:,:][:,i_min:i_max,j_min:j_max]
+    Th[np.where(Th==999.)] = float('nan')
+    temperature_celsius = Th*((p/100000)**(0.4/1.4))-273.15
     # Density
     rhodref = mnhFile.variables['RHOREFZ'][:]
-    rho3D = np.ones(Tc.shape)
-    IKE = Tc.shape[0]
+    density_3D = np.ones(temperature_celsius.shape)
+    IKE = temperature_celsius.shape[0]
     for k in range(IKE):    
-        rho3D[k,:,:]=rhodref[k]
-    return Tc, rho3D
+        density_3D[k,:,:]=rhodref[k]
+    return temperature_celsius, density_3D
 
 
 
@@ -165,12 +159,14 @@ def get_contents(mnhFile:Dataset,
                  hydrometeors:list,
                  name_var_hydro:dict,
                  temperature:np.ndarray,
-                 rho:np.ndarray,
+                 rho3D:np.ndarray,
+                 i_min:int, i_max:int,
+                 j_min:int, j_max:int,
                  )->dict[np.ndarray]:
     contents = {}
     for key in hydrometeors:
         contents[key] = np.empty(temperature.shape)
-        contents[key] = mnhFile.variables[name_var_hydro[key]][0,:,:,:]*rho[:,:,:] # kg/kg of dry air
+        contents[key] = mnhFile.variables[name_var_hydro[key]][0,:,:,:][:,i_min:i_max,j_min:j_max]*rho3D[:,:,:] # kg/kg of dry air
         contents[key][contents[key]==999.] = float('nan')
     return contents
 
@@ -180,18 +176,20 @@ def get_concentrations(mnhFile:Dataset,
                        microphysics_scheme:str,
                        hydrometeors:list,
                        temperature:np.ndarray,
-                       rho:np.ndarray,):
+                       rho:np.ndarray,
+                       i_min:int, i_max:int,
+                       j_min:int, j_max:int,):
     concentrations = {}
     for key in hydrometeors:
         concentrations[key] = np.zeros(temperature.shape)
     
     if microphysics_scheme[0:3]=="ICE":
-        concentrations['ii'] = mnhFile.variables['CIT'][0,:,:,:]
+        concentrations['ii'] = mnhFile.variables['CIT'][0,:,:,:][:,i_min:i_max,j_min:j_max]
         concentrations['ii'][concentrations['ii']==999.] = float('nan')
     if microphysics_scheme[0:3] =="LIM" :
-        concentrations['rr'] = mnhFile.variables['CRAIN'][0,:,:,:] #former name: CRAINT
+        concentrations['rr'] = mnhFile.variables['CRAIN'][0,:,:,:][:,i_min:i_max,j_min:j_max] #former name: CRAINT
         concentrations['rr'][concentrations['rr']==999.]=float('nan')
-        concentrations['ii'] = mnhFile.variables['CICE'][0,:,:,:] #former name: CICET
+        concentrations['ii'] = mnhFile.variables['CICE'][0,:,:,:][:,i_min:i_max,j_min:j_max] #former name: CICET
         concentrations['ii'][concentrations['ii']==999.]=float('nan')
     concentrations['rr']*=rho
     concentrations['ii']*=rho
